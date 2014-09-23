e694d60c7e266e9b911c4f2bcae70fa7aa251a89
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-23 12:16:17 +0100

    Reset working directory after each test - keeps it clean

--- tests.sh
+++ tests.sh
@@ -11,27 +11,29 @@ fi
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TMP_DIR=$DIR/testing_directory
+WORKING_DIR=$DIR/testing_directory
 TESTS_DIR=$DIR/tests
 CMD=../nggroup
 
 # Clean up first - in case the script didn't successfully exit
-rm -rf $TMP_DIR
+rm -rf $WORKING_DIR
 
 # Then set up our new environment and go into it
-mkdir -p $TMP_DIR
-cd $TMP_DIR
+mkdir -p $WORKING_DIR
+cd $WORKING_DIR
 
 ## <Tests>
 
 for test_script in $TESTS_DIR/*.sh;
 do
     env CMD=$CMD $test_script
+    # clean up - no persistent data across tests
+    rm -rf groups sites users
 done
 
 ## </Tests>
 
 # Clean up here if we get this far
-rm -rf $TMP_DIR
+rm -rf $WORKING_DIR
 
 echo "All tests passed"
