c2038bf47fadd8688b9c16168115c092e2d5b4a9
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-23 17:11:35 +0100

    Both (site|group)mod now use same file formats and structure. Tests amended to reflect this

--- tests/1_sitemod.sh
+++ tests/1_sitemod.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
+
+len () {
+    wc -l $1 | cut -f1 -d" "
+}
+
 $CMD siteadd testsite
 $CMD groupadd testgroup
 
 # can add
-$CMD sitemod testsite a testgroup > /dev/null
-grep testgroup "sites/.testsite.rules" > /dev/null
+[ "$(len ./sites/.testsite.rules)" == "0" ]
+$CMD sitemod testsite a gtestgroup > /dev/null
+[ "$(len ./sites/.testsite.rules)" == "1" ]
+grep "@testgroup" "sites/.testsite.rules" > /dev/null
 
 # can't add twice
-! $CMD sitemod testsite a testgroup > /dev/null
+! $CMD sitemod testsite a gtestgroup > /dev/null
+[ "$(len ./sites/.testsite.rules)" == "1" ]
 
 # can delete
-$CMD sitemod testsite d testgroup
-! grep testgroup "sites/.testsite.rules"
+$CMD sitemod testsite d gtestgroup
+! grep testgroup "sites/.testsite.rules"  > /dev/null
 
 # can't re delete
-! $CMD sitemod testsite d testgroup > /dev/null
+! $CMD sitemod testsite d gtestgroup > /dev/null
+
 
 
 # with a valid group
-! $CMD sitemod testsite d testgroup > /dev/null
+! $CMD sitemod testsite d gtestgroup > /dev/null
 # with an invalid group
-! $CMD sitemod testsite d testgroup2 > /dev/null
+! $CMD sitemod testsite d gtestgroup2 > /dev/null
 
 # with an invalid site
-! $CMD sitemod testsite2 d testgroup > /dev/null
+! $CMD sitemod testsite2 d gtestgroup > /dev/null
 
