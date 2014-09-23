3136d80cfd5098410d6b0fdbec72096449b511ff
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-23 12:34:10 +0100

    Added `sitemod .. d` and associated tests

--- tests/sitemod.sh
+++ tests/sitemod.sh
@@ -2,8 +2,15 @@
 $CMD siteadd testsite
 $CMD groupadd testgroup
 
-$CMD sitemod testsite a testgroup
+$CMD sitemod testsite a testgroup > /dev/null
 ! $CMD sitemod testsite a testgroup > /dev/null
+grep testgroup "sites/.testsite.rules" > /dev/null
 
-# TODO: implement sitemod ? d ?
-#! $CMD sitemod testsite d testgroup2
+$CMD sitemod testsite d testgroup
+! grep testgroup "sites/.testsite.rules"
+
+# with a valid group
+! $CMD sitemod testsite d testgroup > /dev/null
+# and without a valid group
+! $CMD sitemod testsite d testgroup2 > /dev/null
+
