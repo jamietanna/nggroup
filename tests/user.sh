f95d9294049fdd39e31e3d5399e758007d516b6a
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-23 12:09:26 +0100

    Amended tests, added user test, TODO re validation

---
+++ tests/user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+$CMD useradd testuser password
+# TODO: have this in global vars
+[ -e "users/testuser" ]
+
+# Know we can't delete non existent
+! $CMD userdel testuser2 > /dev/null
+
+$CMD useradd testuser2 password2
+$CMD userdel testuser2
+
+# cleanup
+$CMD userdel testuser
