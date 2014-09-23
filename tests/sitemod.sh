b86545edb94512d560516ffcab7fba8ed24fa143
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-23 14:45:31 +0100

    Amended sitemod tests

--- tests/sitemod.sh
+++ tests/sitemod.sh
@@ -2,15 +2,26 @@
 $CMD siteadd testsite
 $CMD groupadd testgroup
 
+# can add
 $CMD sitemod testsite a testgroup > /dev/null
-! $CMD sitemod testsite a testgroup > /dev/null
 grep testgroup "sites/.testsite.rules" > /dev/null
 
+# can't add twice
+! $CMD sitemod testsite a testgroup > /dev/null
+
+# can delete
 $CMD sitemod testsite d testgroup
 ! grep testgroup "sites/.testsite.rules"
 
+# can't re delete
+! $CMD sitemod testsite d testgroup > /dev/null
+
+
 # with a valid group
 ! $CMD sitemod testsite d testgroup > /dev/null
-# and without a valid group
+# with an invalid group
 ! $CMD sitemod testsite d testgroup2 > /dev/null
+
+# with an invalid site
+! $CMD sitemod testsite2 d testgroup > /dev/null
 
