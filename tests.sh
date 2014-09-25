2245e7e8b2923af7d1a84db685c11948672a1528
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-25 14:47:31 +0100

    Amended error trap - now shows the file and line the error occurs in

--- tests.sh
+++ tests.sh
@@ -12,7 +12,6 @@ trap 'error_alert $test_script' ERR
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKING_DIR=$DIR/testing_directory
 TESTS_DIR=$DIR/tests
-CMD=../nggroup
 
 # Clean up first - in case the script didn't successfully exit
 rm -rf $WORKING_DIR
@@ -25,7 +24,7 @@ cd $WORKING_DIR
 
 for test_script in $TESTS_DIR/*.sh;
 do
-    env CMD=$CMD $test_script
+    $test_script
     # clean up - no persistent data across tests
     rm -rf groups sites users
 done
