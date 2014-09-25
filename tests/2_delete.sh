6e02e0c892f06a96d7c20760f243fe19966693a3
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-25 15:37:22 +0100

    Added full test for deleting

--- tests/2_delete.sh
+++ tests/2_delete.sh
@@ -12,6 +12,26 @@ $CMD siteadd testsite
 $CMD sitemod testsite a gtestgroup
 $CMD sitemod testsite a utestuser3
 
+# check it removes the actual file
+[ -e users/testuser3 ]
+$CMD userdel testuser3
+[ ! -e users/testuser3 ]
 
+# then that it removes the user from the group
+! grep "^testuser3$" groups/testgroup > /dev/null
+
+# and finally the site
+! grep "^testuser3$" sites/.testsite.rules > /dev/null
+
+
+# check it removes the actual file
+[ -e groups/testgroup ]
+$CMD groupdel testgroup
+[ ! -e groups/testgroup ]
+
+# and from the site
+! grep "^@testgroup$" sites/.testsite.rules > /dev/null
+
+# but not the users from the group
 [ -e users/testuser ]
-$CMD userdel testuser
+[ -e users/testuser2 ]
