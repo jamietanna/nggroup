e9026aa9e4851321155d32346f459501c044ab34
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-27 19:12:56 +0100

    Basic tests now use new config

--- tests/0_user.sh
+++ tests/0_user.sh
@@ -2,9 +2,11 @@
 
 . $(dirname $0)/testing_environ
 
+testuser_path=$(printf $USER_DIR_FORMAT testuser)
+
 $CMD useradd testuser password
 # TODO: have this in global vars
-[ -e "users/testuser" ]
+[ -e "$testuser_path" ]
 
 # Know we can't delete non existent
 ! $CMD userdel testuser2 > /dev/null
