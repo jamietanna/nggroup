497a079cfc54e9ef277c2d1e0645369d7ad71898
Jamie Tanna <jamie@jamietanna.co.uk>
2014-09-29 13:35:41 +0100

    Check that all three fields are set

--- tests/0_user.sh
+++ tests/0_user.sh
@@ -8,12 +8,20 @@ $CMD useradd testuser password test@localhost
 # TODO: have this in global vars
 [ -e "$testuser_path" ]
 
+testuser_file=$(cat $testuser_path)
+p1=$(echo $testuser_file | cut -f1 -d$USER_DIR_FORMAT_DELIM)
+p2=$(echo $testuser_file | cut -f2 -d$USER_DIR_FORMAT_DELIM)
+p3=$(echo $testuser_file | cut -f3 -d$USER_DIR_FORMAT_DELIM)
+
+[ -n "$p1" ]
+[ -n "$p2" ]
+[ -n "$p3" ]
+
 # Know we can't delete non existent
 ! $CMD userdel testuser2 > /dev/null
 
 $CMD useradd testuser2 password2 test2@localhost
 $CMD userdel testuser2
-
 
 # cleanup
 $CMD userdel testuser
