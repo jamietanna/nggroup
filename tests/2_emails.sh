0acb5ce64c5bd0574adbd0ca72a79a6265110c8b
Jamie Tanna <jamie@jamietanna.co.uk>
2014-10-06 21:49:40 +0100

    Added ability to list emails for a given site

--- tests/2_emails.sh
+++ tests/2_emails.sh
@@ -9,6 +9,9 @@ $CMD useradd testuser2 pwd test2@localhost "Test User 2"
 $CMD useradd testuser3 pwd test3@localhost "Test User 3"
 $CMD groupadd testgroup
 $CMD groupmod testgroup a utestuser1
+$CMD siteadd testsite
+$CMD sitemod testsite a utestuser3
+
 
 echo "$($CMD emails)" > "$output_file"
 grep -o "test1@localhost" "$output_file"
@@ -19,6 +22,11 @@ echo "$($CMD emails gtestgroup)" > "$output_file"
 grep -o "test1@localhost" "$output_file"
 ! grep -o "test2@localhost" "$output_file"
 ! grep -o "test3@localhost" "$output_file"
+
+echo "$($CMD emails stestsite)" > "$output_file"
+! grep -o "test1@localhost" "$output_file"
+! grep -o "test2@localhost" "$output_file"
+grep -o "test3@localhost" "$output_file"
 
 $CMD userdel testuser1
 $CMD userdel testuser2
