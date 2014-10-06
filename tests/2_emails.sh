da70becae4a0707a8dc522b117965c60195da720
Jamie Tanna <jamie@jamietanna.co.uk>
2014-10-06 21:21:54 +0100

    Get a list of users' emails (all or by for group)

--- tests/2_emails.sh
+++ tests/2_emails.sh
@@ -2,16 +2,26 @@
 
 . $(dirname $0)/testing_environ
 
-$CMD useradd testuser pwd test@localhost "Test User"
+output_file=./tmp_output_file
+
+$CMD useradd testuser1 pwd test1@localhost "Test User"
 $CMD useradd testuser2 pwd test2@localhost "Test User 2"
 $CMD useradd testuser3 pwd test3@localhost "Test User 3"
+$CMD groupadd testgroup
+$CMD groupmod testgroup a utestuser1
 
-emails_output=$($CMD emails)
-echo $emails_output | grep "test@localhost"  > /dev/null
-echo $emails_output | grep "test2@localhost" > /dev/null
-echo $emails_output | grep "test3@localhost" > /dev/null
+echo "$($CMD emails)" > "$output_file"
+grep -o "test1@localhost" "$output_file"
+grep -o "test2@localhost" "$output_file"
+grep -o "test3@localhost" "$output_file"
 
-$CMD userdel testuser
+echo "$($CMD emails gtestgroup)" > "$output_file"
+grep -o "test1@localhost" "$output_file"
+! grep -o "test2@localhost" "$output_file"
+! grep -o "test3@localhost" "$output_file"
+
+$CMD userdel testuser1
 $CMD userdel testuser2
 $CMD userdel testuser3
 
+rm "$output_file"
