1060d877f50829dae5d9a635703fe3539953326e
Jamie Tanna <jamie@jamietanna.co.uk>
2014-10-24 22:35:02 +0100

    [WIP] Base code for recursive groups - TODO: test full recursion

---
+++ tests/3_group.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/testing_environ
+
+echo "HACK --- @ $0"
+#exit 0
+echo "</HACK> --"
+
+$CMD useradd testuser1 pwd test1@localhost "Test User 1"
+$CMD useradd testuser2 pwd test2@localhost "Test User 2"
+$CMD useradd testuser3 pwd test3@localhost "Test User 3"
+$CMD useradd testuser4 pwd test4@localhost "Test User 4"
+$CMD useradd testuser5 pwd test5@localhost "Test User 5"
+
+$CMD groupadd testgroup1
+$CMD groupadd testgroup2
+$CMD siteadd testsite
+$CMD generate
+debug "-----------"
+$CMD groupmod testgroup1 +testuser1
+$CMD groupmod testgroup1 +testuser2
+$CMD groupmod testgroup2 +@testgroup1
+$CMD groupmod testgroup2 +testuser3
+$CMD sitemod testsite +@testgroup2
+$CMD sitemod testsite +testuser4
+$CMD sitemod testsite +testuser5
+$CMD generate
+
+group1_file=$(printf $GROUP_DIR_FORMAT testgroup1)
+group2_file=$(printf $GROUP_DIR_FORMAT testgroup2)
+
+[ $(wc -l $group1_file | cut -f1 -d" ") -eq 2 ]
+grep "^testuser1$" $group1_file > /dev/null
+grep "^testuser2$" $group1_file > /dev/null
+
+[ $(wc -l $group2_file | cut -f1 -d" ") -eq 2 ]
+grep "^@testgroup1$" $group2_file > /dev/null
+grep "^testuser3$" $group2_file > /dev/null
+
+site_file=$(printf "$SITES_COMPLETE_DIR_FORMAT" "testsite")
+
+grep "^testuser1" $site_file > /dev/null
+grep "^testuser2" $site_file > /dev/null
+grep "^testuser3" $site_file > /dev/null
+grep "^testuser4" $site_file > /dev/null
+grep "^testuser5" $site_file > /dev/null
+
+# error "TODO: TEST GROUP1 -> GROUP2 -> GROUP1 DOESN'T BREAK IT"
