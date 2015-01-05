4054ac4868c1d799bbb944f86014f67a0ed9eb25
Jamie Tanna <jamie@jamietanna.co.uk>
2015-01-05 14:52:50 +0000

    s/len/num_entries - more self explanatory

--- tests/1_groupmod.sh
+++ tests/1_groupmod.sh
@@ -11,20 +11,20 @@ $CMD useradd testuser password test@localhost "Test User"
 $CMD useradd testuser2 password2 test2@localhost "Test User 2"
 
 # can add
-[ "$(len $testgroup_path)" == "0" ]
+[ "$(num_entries $testgroup_path)" == "0" ]
 $CMD groupmod testgroup +testuser
-[ "$(len $testgroup_path)" == "1" ]
+[ "$(num_entries $testgroup_path)" == "1" ]
 grep testuser $testgroup_path  > /dev/null
 ! grep "@testuser" $testgroup_path  > /dev/null
 
 
 # can't readd
 ! $CMD groupmod testgroup +testuser  > /dev/null
-[ "$(len $testgroup_path)" == "1" ]
+[ "$(num_entries $testgroup_path)" == "1" ]
 
 
 $CMD groupmod testgroup +testuser2
-[ "$(len $testgroup_path)" == "2" ]
+[ "$(num_entries $testgroup_path)" == "2" ]
 grep testuser2 $testgroup_path  > /dev/null
 
 # not in the other group
@@ -33,7 +33,7 @@ grep testuser2 $testgroup_path  > /dev/null
 
 
 
-[ "$(len $testgroup2_path)" == "0" ]
+[ "$(num_entries $testgroup2_path)" == "0" ]
 $CMD groupmod testgroup2 +@testgroup
-[ "$(len $testgroup2_path)" == "1" ]
+[ "$(num_entries $testgroup2_path)" == "1" ]
 grep "@testgroup" $testgroup2_path  > /dev/null
