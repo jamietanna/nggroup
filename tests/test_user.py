5d8514b668116354c7e51333a149424ad14dfa80
Jamie Tanna <jamie@jamietanna.co.uk>
2015-06-08 15:48:18 +0100

    Provide tests with constant, shared test data
    Instead of manually adding new sets of data for each test (which in a
    number of cases will be identical), store the data in a couple of
    variables that we can then call from each test.

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,61 +9,94 @@ from user import User, PopulateUser, AddUser, GetUserRulePath
 
 class TestUserObject(unittest.TestCase):
 
+    # TODO make this a `setUpClass` - therefore less work before each test run
+    def setUp(self):
+        users = []
+        userDetails = []
+
+        userDetails.append(
+            (
+                "jamietanna",
+                "******",
+                "jamie@jamietanna.co.uk"
+            )
+            )
+
+        userDetails.append(
+            (
+                "testuser",
+                "******",
+                "test@localhost"
+            )
+            )
+
+        for userDetail in userDetails:
+            users.append(AddUser(
+                userDetail[0],
+                userDetail[1],
+                userDetail[2]
+                ))
+
+        self.userDetails = userDetails
+        self.users = users
+
     def test_saveUserRule(self):
         # Initialise our test data
         NUMBER_PARAMS = 3
-        USERNAME = "jamietanna"
-        PASSWORDHASH = "*******"
-        USEREMAIL = "jamie@jamietanna.co.uk"
 
-        # create our test object
-        AddUser(USERNAME, PASSWORDHASH, USEREMAIL)
+        # TODO abstract out this code into a separate function if possible
+        # i.e. by adding a wrapper that will run a function given the test data
+        for user in self.users:
+            USERNAME = user.username
+            PASSWORDHASH = user.passwordHash
+            USEREMAIL = user.userEmail
 
-        # test that we can save the file correctly
-        userRulePath = GetUserRulePath(USERNAME)
-        with open(userRulePath, "rb") as userRuleFile:
-            csvReader = config.getCSVReader(userRuleFile)
+            # test that we can save the file correctly
+            userRulePath = GetUserRulePath(USERNAME)
+            with open(userRulePath, "rb") as userRuleFile:
+                csvReader = config.getCSVReader(userRuleFile)
 
-            lineCount = 0
-            for line in csvReader:
-                lineCount += 1
-                # we can only have one user per file
-                self.assertEqual(lineCount, 1)
+                lineCount = 0
+                for line in csvReader:
+                    lineCount += 1
+                    # we can only have one user per file
+                    self.assertEqual(lineCount, 1)
 
-                # ensure that we have the correct number of params
-                self.assertEqual(NUMBER_PARAMS, len(line))
-                # and that they're the same that we passed in
-                self.assertEqual(USERNAME, line[0])
-                self.assertEqual(PASSWORDHASH, line[1])
-                self.assertEqual(USEREMAIL, line[2])
+                    # ensure that we have the correct number of params
+                    self.assertEqual(NUMBER_PARAMS, len(line))
+                    # and that they're the same that we passed in
+                    self.assertEqual(USERNAME, line[0])
+                    self.assertEqual(PASSWORDHASH, line[1])
+                    self.assertEqual(USEREMAIL, line[2])
 
-        # remove the file we're testing with
-        os.remove(userRulePath)
-        self.assertFalse(os.path.exists(userRulePath))
+            # remove the file we're testing with
+            os.remove(userRulePath)
+            self.assertFalse(os.path.exists(userRulePath))
 
     def test_populateUser(self):
-        USERNAME = "jamietanna"
-        PASSWORDHASH = "*******"
-        USEREMAIL = "jamie@jamietanna.co.uk"
+        for user in self.users:
+            USERNAME = user.username
+            PASSWORDHASH = user.passwordHash
+            USEREMAIL = user.userEmail
 
-        # create our test object
-        user = AddUser(USERNAME, PASSWORDHASH, USEREMAIL)
+            # create our test object
+            user = AddUser(USERNAME, PASSWORDHASH, USEREMAIL)
 
-        user2 = PopulateUser(USERNAME)
-        # TODO assert equal on getts
-        self.assertEqual(user.username, user2.username)
-        self.assertEqual(user.passwordHash, user2.passwordHash)
-        self.assertEqual(user.userEmail, user2.userEmail)
+            user2 = PopulateUser(USERNAME)
+            # TODO assert equal on getts
+            self.assertEqual(user.username, user2.username)
+            self.assertEqual(user.passwordHash, user2.passwordHash)
+            self.assertEqual(user.userEmail, user2.userEmail)
 
-        # remove the file we're testing with
-        userRulePath = user.getUserRulePath()
-        os.remove(userRulePath)
-        self.assertFalse(os.path.exists(userRulePath))
+            # remove the file we're testing with
+            userRulePath = user.getUserRulePath()
+            os.remove(userRulePath)
+            self.assertFalse(os.path.exists(userRulePath))
 
     def test_properties(self):
-        USERNAME = "jamietanna"
-        PASSWORDHASH = "*******"
-        USEREMAIL = "jamie@jamietanna.co.uk"
+        USERNAME = self.users[0].username
+        PASSWORDHASH = self.users[0].passwordHash
+        USEREMAIL = self.users[0].passwordHash
 
         # create our test object
         user = User(USERNAME, PASSWORDHASH, USEREMAIL)
